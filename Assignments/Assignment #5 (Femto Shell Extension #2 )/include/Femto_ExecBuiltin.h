0389ebd88e8537989152d1e3cf19d4ffaece341e
Ziad <ziad.gamalelden@gmail.com>
2022-10-14 01:10:22 +0200

    Added ExcecBuildin files

---
+++ Assignments/Assignment #5 (Femto Shell Extension #2 )/include/Femto_ExecBuiltin.h
@@ -0,0 +1,11 @@
+#ifndef FEMTO_EXECBUILTIN_H
+#define FEMTO_EXECBUILTIN_H
+
+/**
+* @brief This function searches in builtin commands and execute if matches the input
+*
+* @return 
+*/
+int Femto_ExecBuiltin();
+
+#endif /* FEMTO_EXECBUILTIN_H */
--- Assignments/Assignment #5 (Femto Shell Extension #2 )/src/Femto_Shell/Femto_Shell.c
+++ Assignments/Assignment #5 (Femto Shell Extension #2 )/src/Femto_Shell/Femto_Shell.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "Femto_Shell.h"
+#include "Femto_ExecBuiltin.h"
 
 // Stores the input tokens as arugment variables
 char* femto_argv[FEMTO_MAX_ARGV] = {NULL};
@@ -24,16 +26,34 @@ int Femto_ParseInput()
     // The saveptr is used internally by strtok_r() in order to maintain context between successive calls that parse the same string.
     char* saveptr = NULL; 
     char* token = NULL;
+    char *alloc_token = NULL;
 
+    // tokenizing the input
     saveptr = buff;
-    while((token = strtok_r(saveptr, " ", &saveptr)) != NULL)
+    while((token = strtok_r(saveptr, " \n ", &saveptr)) != NULL)
     {
-        femto_argv[femto_argc++] = token; // token is allocated memory inside strtok_r
+        alloc_token = strdup(token);
+        femto_argv[femto_argc++] = alloc_token; // token is allocated memory inside strdup_
     }
-
+    
+    /* 
+    // For debugging
     for(int i = 0; femto_argv[i] != NULL; i++)
     {
-        printf("argv[i]: %s\n", femto_argv[i]);
+        printf("argv[%d]: %s\n", i, femto_argv[i]);
+    }
+    */
+    return 1;
+}
+
+int Femto_Deconstruct()
+{
+    // Free allocated memory to the tokens
+    while(femto_argc > 0)
+    {
+        femto_argc--;
+        free(femto_argv[femto_argc]);
+        femto_argv[femto_argc] = NULL;
     }
 
     return 1;
@@ -46,9 +66,9 @@ int Femto_Shell()
         if(femto_argc == 0)
             Femto_ParseInput();
         // Femto_Localvar();
-        // Femto_ExecBuiltin();
+        Femto_ExecBuiltin();
         // Femto_ExecExternal();
-        //Femto_Deconstruct();
+        Femto_Deconstruct();
     }
 
     return 1;        
