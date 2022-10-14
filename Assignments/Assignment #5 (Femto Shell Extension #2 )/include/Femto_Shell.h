cdf96219730b8ee94f31eab573bbd905254c9698
Ziad <ziad.gamalelden@gmail.com>
2022-10-14 14:56:49 +0200

    Moved variable definitions to the file.h

--- Assignments/Assignment #5 (Femto Shell Extension #2 )/include/Femto_Shell.h
+++ Assignments/Assignment #5 (Femto Shell Extension #2 )/include/Femto_Shell.h
@@ -1,4 +1,16 @@
+#ifndef FEMTO_SHELL_H
+#define FEMTO_SHELL_H
+
 #define FEMTO_MAX_ARGV 100
+
+// Stores the input tokens as arugment variables
+char* femto_argv[FEMTO_MAX_ARGV];
+
+// Stores the number of tokens
+int femto_argc;
+
+// The current token index
+int token_idx;
 
 /**
 * @brief This function starts the Femto Shell
@@ -14,3 +26,5 @@ int Femto_Shell();
 * @return 1 on sucess
 */
 int Femto_ParseInput();
+
+#endif /* FEMTO_SHELL_H */
