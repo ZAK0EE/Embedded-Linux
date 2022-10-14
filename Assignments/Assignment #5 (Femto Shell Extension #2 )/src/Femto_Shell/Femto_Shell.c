83267740303b16092ada293e42fb195637cebd89
Ziad <ziad.gamalelden@gmail.com>
2022-10-14 14:57:32 +0200

    Added \t to the delimiters

--- Assignments/Assignment #5 (Femto Shell Extension #2 )/src/Femto_Shell/Femto_Shell.c
+++ Assignments/Assignment #5 (Femto Shell Extension #2 )/src/Femto_Shell/Femto_Shell.c
@@ -8,6 +8,7 @@
 // Stores the input tokens as arugment variables
 char* femto_argv[FEMTO_MAX_ARGV] = {NULL};
 
+
 // Stores the number of tokens
 int femto_argc = 0; 
 
@@ -19,7 +20,7 @@ int Femto_ParseInput()
     char buff[500] = {0};
 
     printf("Please enter your command > ");
-    
+
     if((fgets(buff, sizeof(buff), stdin) == NULL))
         return 0;
 
@@ -30,19 +31,18 @@ int Femto_ParseInput()
 
     // tokenizing the input
     saveptr = buff;
-    while((token = strtok_r(saveptr, " \n ", &saveptr)) != NULL)
+    while((token = strtok_r(saveptr, " \t\n", &saveptr)) != NULL)
     {
         alloc_token = strdup(token);
         femto_argv[femto_argc++] = alloc_token; // token is allocated memory inside strdup_
     }
-    
-    /* 
+
     // For debugging
     for(int i = 0; femto_argv[i] != NULL; i++)
     {
         printf("argv[%d]: %s\n", i, femto_argv[i]);
     }
-    */
+
     return 1;
 }
 
