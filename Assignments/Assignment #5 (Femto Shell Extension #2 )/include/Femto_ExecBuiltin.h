f61cd08528dbfa41fd5fb031d2eae0c76fffe16a
Ziad <ziad.gamalelden@gmail.com>
2022-10-14 14:57:56 +0200

    Added the library builtin comands

--- Assignments/Assignment #5 (Femto Shell Extension #2 )/include/Femto_ExecBuiltin.h
+++ Assignments/Assignment #5 (Femto Shell Extension #2 )/include/Femto_ExecBuiltin.h
@@ -1,10 +1,15 @@
 #ifndef FEMTO_EXECBUILTIN_H
 #define FEMTO_EXECBUILTIN_H
 
+typedef struct
+{
+    char* name;
+    int (*call)();
+} command;
 /**
 * @brief This function searches in builtin commands and execute if matches the input
 *
-* @return 
+* @return 1 on sucess and 0 on failure 
 */
 int Femto_ExecBuiltin();
 
--- Assignments/Assignment #5 (Femto Shell Extension #2 )/src/Femto_Shell/Femto_ExecBuiltin.c
+++ Assignments/Assignment #5 (Femto Shell Extension #2 )/src/Femto_Shell/Femto_ExecBuiltin.c
@@ -1,5 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include "Femto_ExecBuiltin.h"
+#include "Femto_Shell.h"
+
+#include "libinclude/femto_rand.h"
+#include "libinclude/femto_fib.h"
+#include "libinclude/femto_fact.h"
+
+command builtin_commands[] =
+{
+    {"rand" ,femto_rand},
+    {"fib", femto_fib},
+    {"fact", femto_fact},
+    {NULL, NULL}
+};
+
+
 int Femto_ExecBuiltin()
 {
 
+    for(int i = 0; builtin_commands[i].name != NULL; i++)
+    {
+        if(strcmp(femto_argv[token_idx], builtin_commands[i].name) == 0)
+        {
+//            printf("MATCHED to: %s\n", builtin_commands[i].name);
+            builtin_commands[i].call();
+            break;
+        }
+    }
+    
+    return 1;
 }
 
