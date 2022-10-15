dbc16e71ef8fea7eec3b5bae1f43fe34c15495e3
Ziad <ziad.gamalelden@gmail.com>
2022-10-15 14:54:54 +0200

    Added simple executing external command implementation

--- Assignments/Assignment #5 (Femto Shell Extension #2 )/src/Femto_Shell/Femto_ExecExternal.c
+++ Assignments/Assignment #5 (Femto Shell Extension #2 )/src/Femto_Shell/Femto_ExecExternal.c
@@ -3,13 +3,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "Femto_ParseInput.h"
 
 int Femto_ExecExternal()
 {
-    execvpe(femto_argv[token_idx], &femto_argv[token_idx], NULL);
-    printf("This msg won't be printed on sucess\n");
+    int status = 0;
+    int ret_pid = fork();
+
+    if(ret_pid < 0)
+    {
+        printf("This msg won't be printed on sucess\n");
+    }
+    else if(ret_pid > 0)
+    {
+        wait(&status);
+    }
+    else if(ret_pid == 0)
+    {
+        execvpe(femto_argv[token_idx], &femto_argv[token_idx], NULL);
+    }
+
 
     return 1;
 }
