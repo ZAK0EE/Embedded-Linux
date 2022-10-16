196c9651c6cee54922ba6719d6819523b9821662
Ziad <ziad.gamalelden@gmail.com>
2022-10-16 03:07:11 +0200

    Added cmd loop and also zeroing the used command

--- Assignments/Assignment #5 (Femto Shell Extension #2 )/src/Femto_Shell/fem_shell.c
+++ Assignments/Assignment #5 (Femto Shell Extension #2 )/src/Femto_Shell/fem_shell.c
@@ -21,18 +21,22 @@ int fem_deconstruct()
 int fem_shell()
 {
     while(1)
-    {
+    {	
         if(!fem_parser_input())
             continue;
         // fem_Localvar();
-        for(int i = parser_cmdidx; i < parser_cmdc; i++)
+
+        char **cmd;
+        while( (cmd = parser_getNextcmd()) != NULL)
         {
-		    fem_exec_builtin(*parser_cmdv[i]);
-		    fem_exec_external(*parser_cmdv[i],  parser_cmdv[i]);        
+		    fem_exec_builtin(*cmd);
+		    fem_exec_external(*cmd,  cmd);                   
+		    parser_cmdv[parser_cmdidx - 1] = NULL; // to null it after the usage
         }
         
-		parser_cmdidx = 0;
+        parser_cmdidx = 0;
 		parser_cmdc = 0;
+		
         fem_deconstruct();
     }
 
